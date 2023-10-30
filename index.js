ca45d5080d7fd5a70bbec19179fe6f94f32162fc
ALUNO D26 <alunod26@acad.unifor.br>
2023-10-30 17:38:34 -0300

    Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>alteração trabalho!</h1>");
+    res.send("<h1>APRESENTAÇÃO TRABALHO!</h1>");
 })
 
 //rota da equipe
