bbd27166ab515ea5777b84c32c5b324c40097eb8
ALUNO D26 <alunod26@acad.unifor.br>
2023-10-30 17:36:34 -0300

    alteração

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>SEGUNDO TRABALHO NUVEM!</h1>");
+    res.send("<h1>alteração trabalho!</h1>");
 })
 
 //rota da equipe
